60b4631bdf5f86355b70dc0b14b24ae57bc44e20
Abhishek Kumar <techieabhik@gmail.com>
2025-12-10 13:42:29 +0530

    Initial commit

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+
+/*
+* psvm is shortcut for main method
+* sout is for System.out.println
+*/
+    static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println(1234);
+
+    }
+}
