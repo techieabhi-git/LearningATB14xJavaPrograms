dfed7aa1944fc9929bce1a5f6db568932d558bc0
Abhishek Kumar <techieabhik@gmail.com>
2025-12-10 22:15:08 +0530

    Commit

--- src/ex_02_Java_Basics/Lab002_HelloWorld.java
+++ src/ex_02_Java_Basics/Lab002_HelloWorld.java
@@ -1,4 +1,11 @@
 package ex_02_Java_Basics;
 
 public class Lab002_HelloWorld {
+    static void main() {
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+    }
+
 }
