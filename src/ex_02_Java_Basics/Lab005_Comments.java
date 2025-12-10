1049d320510260715444f9626a1b7dad142657fb
Abhishek Kumar <techieabhik@gmail.com>
2025-12-10 22:33:06 +0530

    Commit

---
+++ src/ex_02_Java_Basics/Lab005_Comments.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab005_Comments {
+
+    static void main() {
+
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        System.out.println("Hello World!");
+    }
+}
