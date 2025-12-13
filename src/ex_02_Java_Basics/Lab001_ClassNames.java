8aeaf0fcf7fd277c252fc0980d6b9735819bbfaa
Abhishek Kumar <techieabhik@gmail.com>
2025-12-14 00:00:46 +0530

    Classes Names

--- src/ex_02_Java_Basics/Lab001_ClassNames.java
+++ src/ex_02_Java_Basics/Lab001_ClassNames.java
@@ -2,5 +2,32 @@ package ex_02_Java_Basics;
 
 public class Lab001_ClassNames {
 
-    // Class name can start with $
+    // Only 1 Public class is allowed in JAVA
 }
+
+class $Abhishek {
+
+    // Class name can start with $
+
+    public static class Enum {
+
+        /*
+        The “only one public class per file” rule applies ONLY to top-level classes.
+        A nested (inner) class is NOT a top-level class, so the rule does not apply to it
+        */
+
+    }
+}
+
+class Enum{
+
+    // valid class name, Enum is valid because we changed small E with capital E in enum
+}
+
+class ABC123{
+
+    //Valid class name
+}
+
+
+
